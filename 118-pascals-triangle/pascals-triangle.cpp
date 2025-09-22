325813abc74ea2bff87a7f072868fda184a02876
Swadeep Dhondi <113923684+Swadeep006@users.noreply.github.com>
2025-09-22 18:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (64.50%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,12 +1,7 @@
 vector<vector<long>> dp(31, vector<long>(31, 1));
 class Solution {
 public:
-    // vector<vector<int>> dp(31,vector<int>(31,1));
     void pascals() {
-        for (long i = 0; i < 31; i++) {
-            dp[i][0] = 1; // first element of each row
-            dp[i][i] = 1; // last element of each row
-        }
         for (long i = 2; i < 31; i++) {
             for (long j = 1; j < i; j++) {
                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
@@ -17,7 +12,6 @@ public:
         pascals();
         vector<vector<int>> ans;
         for (int i = 0; i < numRows; i++) {
-            // Insert first i+1 elements from dp[i] directly
             ans.push_back(vector<int>(dp[i].begin(), dp[i].begin() + i + 1));
         }
         return ans;
