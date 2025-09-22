4fe56cb71983d0e49c1326bac8cd4d8a63b1ebfe
Swadeep Dhondi <113923684+Swadeep006@users.noreply.github.com>
2025-09-22 18:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (64.50%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -8,11 +8,15 @@ public:
             }
         }
     }
-    vector<vector<int>> generate(int numRows) {
+     vector<vector<int>> generate(int numRows) {
         pascals();
         vector<vector<int>> ans;
         for (int i = 0; i < numRows; i++) {
-            ans.push_back(vector<int>(dp[i].begin(), dp[i].begin() + i + 1));
+            vector<int> res;
+            for (int j = 0; j <= i; j++) {
+                res.push_back(dp[i][j]); 
+            }
+            ans.push_back(res);
         }
         return ans;
     }
